8854ebe26df5b2eeaeba412f7866f112817554e0
Vaibhav Yadav <yvaibhav756@gmail.com>
2024-01-05 21:39:23 +0530

    SSO Application added.

---
+++ SSOSocialApp/src/main/java/com/SsoSocialAppApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoSocialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoSocialAppApplication.class, args);
+	}
+
+}
